ab5341ee704ca32bdcf7718e9a0addae10312571
Yuriel849 <yuriel849sp@gmail.com>
2019-02-08 18:22:26 +0900

    file upload complete

--- OpenProject_Spring/src/main/java/com/yuriel/service/LoginServiceImpl.java
+++ OpenProject_Spring/src/main/java/com/yuriel/service/LoginServiceImpl.java
@@ -1,7 +1,5 @@
 package com.yuriel.service;
 
-
-import javax.annotation.Resource;
 import javax.inject.Inject;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -19,18 +17,13 @@ public class LoginServiceImpl implements LoginService {
 	private UserMapper mapper;
  // private UserMapper mapper = template.getMapper(UserMapper.class); -> IMPOSSIBLE! -> DI는 객체 생성 후 실행, 이것은 생성할 떄 실행
 	
-//	// servlet-context.xml에 설정한 uploadPath 빈 주입하기
-//	@Resource(name = "uploadPath")
-//	private String uploadPath;
-	
 	@Override
 	public UserVO login(UserVO vo) throws Exception {
-		String uploadPath = "/upload/";
+		String uploadPath = "../resources/photos/";
 
 		mapper = template.getMapper(UserMapper.class);
 		UserVO result = mapper.login(vo);
-		result.setFullName("file:C:/Users/Yuriel/Downloads/Upload/2019/02/08/e6eb8c51-a40b-4451-ad3d-d409ac632894_Sunwapta Falls, Canada.jpg");
-		System.out.println("THIS IS THE LOGIN SERVICE IMPL CLASS... THE RESULT OF LOGIN IS : " + result);
+		result.setFullName(uploadPath + result.getFullName());
 		return result;
 	}	
 }
--- OpenProject_Spring/src/main/java/com/yuriel/service/UserServiceImpl.java
+++ OpenProject_Spring/src/main/java/com/yuriel/service/UserServiceImpl.java
@@ -1,13 +1,11 @@
 package com.yuriel.service;
 
 import java.io.File;
-import java.text.DecimalFormat;
-import java.util.Calendar;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,10 +26,6 @@ public class UserServiceImpl implements UserService {
 	private UserMapper mapper;
  // private UserMapper mapper = template.getMapper(UserMapper.class); -> IMPOSSIBLE! -> DI는 객체 생성 후 실행, 이것은 생성할 떄 실행
 	
-//	// servlet-context.xml에 설정한 uploadPath 빈 주입하기
-//	@Resource(name = "uploadPath")
-//	private String uploadPath;
-	
 	@Transactional
 	@Override
 	public int createUser(UserVO vo, HttpServletRequest request) throws Exception {
@@ -39,9 +33,9 @@ public class UserServiceImpl implements UserService {
 		UUID uid = UUID.randomUUID();
 		
 		// 저장될 경로를 계산한다.
-		String uploadPath = "/upload/";
-		String datePath = calcPath(uploadPath);
-		String savedPath = uploadPath + datePath;
+		String uploadPath = "/resources/photos/";
+		String dir = request.getSession().getServletContext().getRealPath(uploadPath);
+		
 		// 파일명에 UUID를 더한다.
 		String saveFileName = uid.toString() + "_" + vo.getImg().getOriginalFilename();
 		
@@ -51,40 +45,13 @@ public class UserServiceImpl implements UserService {
 		
 		if(!vo.getImg().isEmpty()) { // UserVO 객체의 img 변수가 비어있지 않는다면...
 			// 사용자가 업로드한 파일을 지정된 위치로 이동시킨다. 
-			vo.getImg().transferTo(new File(savedPath, saveFileName));
-			// 파일명에 년/월/일 경로 정보를 붙인다.
-			String imgName = datePath + File.separator + saveFileName;
-			System.out.println("imgName : " + imgName);
-			// users_attach 테이블에 파일명을 사용자 ID(auto_increment)와 함께 추가한다 -> 파일명은 년/월/일 + UUID + 원래 파일이름이다!
+			vo.getImg().transferTo(new File(dir, saveFileName));
+			String imgName = saveFileName;
+			// users_attach 테이블에 파일명을 사용자 ID(auto_increment)와 함께 추가한다.
 			mapper.addAttach(imgName);
 		}
 		
 		return result;
-	}
-	
-	// 년, 월, 일로 세분화할 수 있도록 경로 계산
-	private static String calcPath(String uploadPath) {
-		Calendar cal = Calendar.getInstance();
-
-		String yearPath = File.separator + cal.get(Calendar.YEAR);		
-		String monthPath = yearPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.MONTH) + 1);
-		String datePath = monthPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.DATE));
-		
-		// 지정된 저장위치에 년/월/일 경로를 만든다 (년/월/일에 해당하는 디렉토리가 있는지 확인하고 없다면 추가한다.)
-		makeDir(uploadPath, yearPath, monthPath, datePath);
-		
-		return datePath;
-	}
-	
-	// 년, 월, 일로 세분화할 수 있도록 추가 디렉토리 생성
-	private static void makeDir(String uploadPath, String... paths) {
-		if(new File(uploadPath + paths[paths.length - 1]).exists()) { return; }
-		
-		for(String path : paths) {
-			File dirPath = new File(uploadPath + path);
-			// dirPath에 해당하는 경로가 존재하지 않는다면 새롭게 디렉토리 생성
-			if(!dirPath.exists()) { dirPath.mkdir(); }
-		}
 	}
 	
 	@Override
