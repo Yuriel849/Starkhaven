949c7419169d1dd22cf5b00509397ec48dfb15d1
Yuriel849 <yuriel849sp@gmail.com>
2019-02-14 00:04:03 +0900

    minor updates

--- OpenProject_Spring/src/main/java/com/yuriel/controller/ModifyController.java
+++ OpenProject_Spring/src/main/java/com/yuriel/controller/ModifyController.java
@@ -6,8 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.yuriel.domain.UserListVO;
 import com.yuriel.domain.UserVO;
@@ -21,7 +20,7 @@ public class ModifyController {
 	private UserService service;
 	
 	// GET -> 항상 사용자가 직접 브라우저에서 접근 가능한 경우 사용한다.
-	@RequestMapping(value ="/modifyUser", method = RequestMethod.POST)
+	@PostMapping("/modifyUser")
 	public String modifyPOST(UserVO user, int countPerPage, int pageNumber, Model model) throws Exception {
 		logger.info("******************** modify user POST ********************");
 		
