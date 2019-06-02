212aeb38e004ed3eca35848a725d0ca9c7cdc506
Yuriel849 <yuriel849sp@gmail.com>
2019-06-02 15:18:01 +0900

    fix errors

--- 01. Node.js 교과서/05._Sequelize/models/user.js
+++ 01. Node.js 교과서/05._Sequelize/models/user.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             defaultValue: DataTypes.NOW,
         },
-    }), {
+    }, {
         timestamps: false,
-    };
+    });
 };
