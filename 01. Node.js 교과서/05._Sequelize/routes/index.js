11a51005f75b5fdf569383922790ff82f634dea9
Yuriel849 <yuriel849sp@gmail.com>
2019-06-04 11:42:53 +0900

    app & routing js

--- 01. Node.js 교과서/05._Sequelize/routes/index.js
+++ 01. Node.js 교과서/05._Sequelize/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var User = require('../models').User;
 var router = express.Router();
 
-/* GET home page. */
+/* GET "/" => find all data in table "users" and then render sequelize.pug, provide "users" as arguments */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  User.findAll()
+    .then((users) => { // Sequelize supports Promise, so this is possible
+      res.render('sequelize', { users });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
 });
+
+/* async/await instead of Promise */
+// router.get('/', async(req, res, next) => {
+//   try {
+//     const users = await User.findAll();
+//     res.render('sequelize', { users });
+//   } catch(error) {
+//     console.error(error);
+//     next(error);
+//   }
+// });
+
 
 module.exports = router;
