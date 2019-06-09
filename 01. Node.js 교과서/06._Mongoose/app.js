4bffe990f41651ce569abd560376faee1b30c4fc
Yuriel849 <yuriel849sp@gmail.com>
2019-06-09 12:15:58 +0900

    error fix

--- 01. Node.js 교과서/06._Mongoose/app.js
+++ 01. Node.js 교과서/06._Mongoose/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var commentsRouter = require('./routes.comments');
+var commentsRouter = require('./routes/comments');
 var connect = require('./schemas');
 
 var app = express();
